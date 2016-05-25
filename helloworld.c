c56c120e5378f3b8b2dcf0fe546aaa3546a4afc4
ewangclarkson <ewangclarks@gmail.com>
2016-05-25 14:49:38 +0100

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+//includes the declarations of functions needed for the program to execute properly
+#include<stdio.h>
+
+//definition of the mandatory function
+int main(void)
+ {
+   //display a simple Hello World text
+   
+     printf("Hello World\n");
+     
+     return 0;
+  }
+  //end of code
+
